594ee9ef0009f57e35af1943a259d7c59b894a57
polygoning <polygoning@gmail.com>
2016-07-19 22:32:24 -0400

    Add local library loading, add placeholder signin fields

--- app/views/SignIn.js
+++ app/views/SignIn.js
@@ -2,12 +2,32 @@ var React = require('react')
 
 var textStyle = {
   textAlign: "center"
-}
+},
+btnStyle = {
+  marginTop: "25px"
+},
+divStyle = {
+    padding: "40px 15px",
+    textAlign: "center"
+};
 
 var SignIn = React.createClass({
   render: function(){
     return (
-      <h1 style={textStyle}>Sign In</h1>
+      <div>
+        <h1 style={divStyle}>Sign In</h1>
+        <form>
+          <fieldset className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" placeholder="Username"/>
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Options</label>
+            <input type="password" className="form-control" placeholder="Password"/>
+        </fieldset>
+        <button className="btn btn-info" style={btnStyle} type="submit">Create!</button>
+        </form>
+      </div>
     )
   }
 });
